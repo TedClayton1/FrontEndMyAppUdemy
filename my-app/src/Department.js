936a830b96094e424ed59fc259eb35be2764f15a
TedClayton1 <ted_clayton@hotmail.com>
2022-03-27 13:02:21 -0400

    Had an error message Unterminated JSX contents which was resolved by adding a closing div tag.

--- my-app/src/Department.js
+++ my-app/src/Department.js
@@ -176,23 +176,50 @@ export class Department extends Component{
        <th>
            <div className="d-flex flex-row">
 
-
            
            <input className="form-control m-2"
            onChange={this.changeDepartmentIdFilter}
            placeholder="Filter"/>
 
+           <button type="button" className="btn btn-light"
+           onClick={()=>this.sortResult('DepartmentId',true)}>
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+               <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
+               </svg>               
+           </button> 
+
+           <button type="button" className="btn btn-light"
+           onClick={()=>this.sortResult('DepartmentId',false)}>               
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+               <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+               </svg>              
+           </button> 
 
            </div> 
            DepartmentId
        </th>
        <th>
-           <input className="form-control m-2"
+       <div className="d-flex flex-row">
+       <input className="form-control m-2"
            onChange={this.changeDepartmentNameFilter}
            placeholder="Filter"/>
+
+<button type="button" className="btn btn-light"
+           onClick={()=>this.sortResult('DepartmentId',true)}>
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+               <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
+               </svg>               
+           </button> 
+
+           <button type="button" className="btn btn-light"
+           onClick={()=>this.sortResult('DepartmentId',false)}>               
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+               <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+               </svg>              
+           </button>         
            </div>
-           
            DepartmentName
+           
        </th>       
        <th>
            Options
@@ -290,6 +317,7 @@ export class Department extends Component{
     
 </div>
 
+</div>
 </div>
 </div>
 
